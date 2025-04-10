4e120599184f8f2c0f62aaba0f2a19ccd9bb70a5
sauheiwong <sauheiwong@gmail.com>
2025-04-10 16:25:25 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+require("dotenv").config();
+
+app.get('/', (req, res) => {
+  res.status(200).send(`🚚 Welcome to the Food Truck!
+    Here is our meun:
+        1. /hours
+        2. /contact
+        3. /hot food`);
+});
+
+app.get('/hours', (req, res) => {
+    res.status(200).send(
+        '🕒 We are open from 11am to 7pm!'
+    )
+})
+
+app.get('/contact', (req, res) => {
+    res.status(200).send(
+        `<p style='color: green'>
+        📞 Contact us at 555-FOOD or hello@foodtruck.com
+        </p>`
+    )
+})
+
+app.get('/menu', (req, res) => {
+    res.status(200).send(
+       `<html><head><title>Menu</title></head><body>
+            <h1>🥙 Our Menu</h1>
+            <ul>
+              <li>Tacos</li>
+              <li>Quesadillas</li>
+              <li>Smoothies</li>
+            </ul>
+          </body></html>` 
+    )
+})
+
+app.listen(process.env.PORT_NUMBER, () => {
+  console.log('Server running on http://localhost:3000');
+});
